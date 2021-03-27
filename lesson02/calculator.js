de547877a4f665a8f33aa138fa7417c46acc24c9
Lauren Weldon <laweldon@microsoft.com>
2021-03-26 18:21:35 -0700

    Built a calculator

---
+++ lesson02/calculator.js
@@ -0,0 +1,9 @@
+let x = '24';
+let y = 16;
+const w = 8
+var z = '12';
+
+console.log("Opperation 1: "+x+y);
+console.log("Opperation 2: "+x*w);
+console.log("Opperation 3: "+z/w);
+console.log("Opperation 4: "+x-z);
